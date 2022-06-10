074ae923064dc07b7e1e86423f595b3eec6d7233
Randsson Silva <randsson.silva@locaweb.com.br>
2022-06-10 00:44:30 -0300

    add Store service

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const Api = axios.create({baseURL: 'https://13f6-2804-d49-4708-1500-6bd3-7e2-8c99-ed54.sa.ngrok.io/api/v1'})
+const Api = axios.create({baseURL: 'https://fdcf-2804-d49-4708-1500-5cdd-d49b-c6cf-2616.sa.ngrok.io/api/v1'})
 
 export default Api;
