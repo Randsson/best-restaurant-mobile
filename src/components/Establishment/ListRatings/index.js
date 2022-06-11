e55a02f665545831098a2db5f8dafd21982ea89f
Randsson Silva <randsson.silva@locaweb.com.br>
2022-06-11 17:50:02 -0300

    add list rating compoment

---
+++ src/components/Establishment/ListRatings/index.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import StoreService from '../../../services/store_service';
+import EstablishmentService from '../../../services/establishment_service';
+import StarRating from 'react-native-star-rating';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const ListRatings = (props) => {
+  const [store, setStore] = useState([]);
+
+  useEffect(() => {
+    getStore();
+  }, [props.place]);
+
+  async function getStore() {
+    try {
+      const response = await EstablishmentService.show(props.place.place_id);
+      // console.log(Object.keys(response.data.result));
+      setStore(response.data.result);
+    } catch (error) {
+      setStore([]);
+    }
+  }
+  
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <Text style={styles.opinions}>
+        {(store.user_ratings_total > 0) ? store.user_ratings_total : '0'} Opiniões
+      </Text>
+      <StarRating
+        disabled={true}
+        maxStars={5}
+        rating={store.rating}
+        fullStarColor="yellow"
+        starSize={15}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: 'white',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginHorizontal: 30,
+  },
+  opinions: {
+    color: 'white',
+    marginLeft: 10,
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginRight: 20,
+  },
+  user_name: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginRight: 30,
+  },
+  text: {
+    color: 'white',
+    marginHorizontal: 20,
+    fontSize: 10,
+  },  
+});
+
+export default ListRatings;
